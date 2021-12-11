261714598499fb04d2eeddea2b275f1c0090bd78
David Wong <40117935+draflier@users.noreply.github.com>
2021-12-11 20:58:39 +0800

    create user api

--- PriceBackend/src/controller/app.controller.ts
+++ PriceBackend/src/controller/app.controller.ts
@@ -1,12 +1,21 @@
 import { AppService } from "@/service/app.service";
-import { Controller, Get , Post} from "@nestjs/common";
+import { Body, Controller, Get , Post} from "@nestjs/common";
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiPayloadTooLargeResponse,
+  ApiResponse,
+  ApiTags,
+  ApiParam,
+  ApiQuery
+} from '@nestjs/swagger';
 
 @Controller("api/setTgUsername")
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Post()
-  createUser(): string {
-    return this.appService.createUser("Smilalot");
+  @Post('/create-user')
+  createUser(@Body() strUsername: string) : string {
+    return this.appService.createUser(strUsername);
   }
 }
